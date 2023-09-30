b5f8a0169376a8987e760194571494661c1b5780
Rogers Hornsby <rogers.hornsby@whitehouse.gov>
2023-09-30 15:54:50 -0400

    initial commits

---
+++ api.py
@@ -0,0 +1,15 @@
+import selenium
+
+class API:
+    def __init__(self,username, password):
+        self.username = username
+        self.password = password
+
+    def login(self):
+        return 'TODO'
+    
+    def logout(self):
+        return 'TODO'
+    
+    def load_config(self):
+        return 'TODO'
